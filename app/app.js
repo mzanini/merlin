d60ad6180a3af0887321af7418b6f4ea7ccee78f
Marco Zanini <chetto87@gmail.com>
2018-01-01 12:38:14 -0800

    Add bootstrap and initial styling

--- app/app.js
+++ app/app.js
@@ -5,15 +5,24 @@ import { Link } from 'react-router-dom';
 import SingleCharacterRolling from './components/single-character-rolling';
 import NewGame from './components/NewGame';
 import Game from './components/Game';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Root = () => {
 return (
   <MemoryRouter
     initialEntries={[ '/', '/game']}
     initialIndex={0}>
-    <div>
-    <Link to="/"> homepage </Link>
-    <Link to="/new-game"> New game </Link>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm">
+          <Link to="/"> Homepage </Link>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          <Link to="/new-game"> New game </Link>
+        </div>
+      </div>
       <Switch>
         <Route exact path="/"/>
         <Route path="/new-game" component={NewGame} />
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -19,7 +19,9 @@ module.exports = {
   devtool: debug ? "inline-sourcemap" : null,
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: ['url-loader'] }
     ]
   }
 };

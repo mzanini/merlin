719d2ec356cc911a108b12f9553c309b8dc46e02
Marco Zanini <chetto87@gmail.com>
2017-12-31 16:30:51 -0800

    Use react router to change main page

--- app/app.js
+++ app/app.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { MemoryRouter, Switch, Route } from 'react-router';
+import { Link } from 'react-router-dom';
 import SingleCharacterRolling from './components/single-character-rolling';
 import Homepage from './components/Homepage';
 
 const Root = () => {
 return (
-    <MemoryRouter>
-        <div>
-            <Switch>
-                <Route path="/" component={Homepage} />
-            </Switch>
-        </div>
-    </MemoryRouter>
+  <MemoryRouter
+    initialEntries={[ '/', '/game']}
+    initialIndex={0}>
+    <div>
+    <Link to="/"> homepage </Link>
+    <Link to="/game"> game </Link>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/game" component={SingleCharacterRolling} />
+      </Switch>
+    </div>
+  </MemoryRouter>
 )
 }
 

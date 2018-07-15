d05e4531fef19f52de8c5c7b2ae9410359796b47
Marco Zanini <marco@grooveapp.com>
2018-07-14 22:07:01 -0700

    Delete Game test

--- test/Game.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Game from '../app/components/Game'
-
-test('starts with empty game name and info', () => {
-    const game = shallow(<Game/>)
-    //expect(game.state('name')).toEqual('');
-})

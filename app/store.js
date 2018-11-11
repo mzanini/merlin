a177481ee47bf2e8b7044466b3ee6824e12a1ec3
Marco Zanini <marco@grooveapp.com>
2018-11-11 15:52:29 -0800

    Add redux devtools

--- app/store.js
+++ app/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import myReducer, {initialState} from './reducers'
+import myReducer, { initialState } from './reducers'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   myReducer,
   initialState,
-  compose(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk)),
 )
 
 export default store
